a97c41a32b7d6560d20941b85aaca5cfed95eb98
ariefkoko <arief.renaldy16@gmail.com>
2021-07-30 09:05:17 +0700

    pesan commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 3000;
+const port = process.env.PORT;
 const router = require('./routes')
 var session = require('express-session')
 app.use(session({
